07c736eb316498829f34cfbc6898f97b4373b965
Parva Shah <parvashah310501@gmail.com>
2022-01-31 18:40:55 +0530

    for loop

--- src/App.js
+++ src/App.js
@@ -5,10 +5,24 @@ import {Todos} from './MyComponents/Todos';
 
 
 function App() {
+  let todos = [
+  {  sno: 1,
+    title: "Go to the market",
+    desc: "You need to got to market to buy fruits"
+  },
+  {  sno: 2,
+    title: "Go to the Gym",
+    desc: "Hit the Gym today"
+  },
+  {  sno: 2,
+    title: "Go to the movies",
+    desc: "Watch spiderman"
+  }
+  ]
   return (
     <>
       <Header title="My Todos List" searchBar={false}/>
-      <Todos/>
+      <Todos todos={todos}/>
       <Footer/>
     </>
   );
--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { TodoItem } from './TodoItem';
 
-export const Todos = () => {
+export const Todos = (props) => {
   return(
-    <div>
-        Todos Work!!
+    <div className='container'>
+      <h3 className="text-center my-3">Todos List</h3>
+      <TodoItem todo={props.todos[0]}/>
     </div>
   );
 };
